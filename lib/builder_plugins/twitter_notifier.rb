94f6cf747bf240d421fd94dd2b37be03e25509cb
Emerson Macedo <emerleite@gmail.com>
2009-06-22 16:14:27 -0300

    twitter plugin

---
+++ lib/builder_plugins/twitter_notifier.rb
@@ -0,0 +1,45 @@
+# Send twitter notifications when build fail or fix
+#
+# 1. Configure Twitter account to broadcast messages:
+# <pre><code>Project.configure do |project|
+#   ...
+#   project.twitter_notifier.username = "youruser"
+#   project.twitter_notifier.password = "yourpassword"
+#   ...
+# end</code></pre>
+
+require 'rubygems'
+require 'twitter'
+
+class TwitterNotifier
+  attr_writer :username
+  attr_writer :password
+
+  def initialize(project = nil)
+  end
+
+  def build_finished(build)
+    return if @username.empty? or @password.empty? or not build.failed?
+    tweet build, "#{build.project.name} build #{build.label} failed"
+  end
+
+  def build_fixed(build, previous_build)
+    return if @username.empty? or @password.empty?
+    tweet build, "#{build.project.name} build #{build.label} fixed"
+  end
+
+  private
+
+  def tweet(build, message)
+    httpauth = Twitter::HTTPAuth.new(@username, @password)
+    base = Twitter::Base.new(httpauth)
+    base.update(message)
+    CruiseControl::Log.event("Build tweet to #{@username} - #{message}", :debug)
+  rescue => e
+    CruiseControl::Log.event("Error while tweet to #{@username}", :error)
+    raise
+  end
+
+end
+
+Project.plugin :twitter_notifier
